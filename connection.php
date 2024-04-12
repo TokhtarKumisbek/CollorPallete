e9a060e6f6cf4e4678fe166259dbd5e97b12bf46
Tokhtar Kumisbek <162350320+TokhtarKumisbek@users.noreply.github.com>
2024-04-12 07:22:23 +0500

    connection.php

--- connection.php
+++ connection.php
@@ -2,16 +2,16 @@
     $hostname = "localhost";
     $db_username = "root";
     $db_password = "";
-    $db_name = "registeruser"; // Replace with your actual database name
+    $db_name = "registeruser"; 
 
-    // Create connection
+
     $mysqli = new mysqli($hostname, $db_username, $db_password, $db_name);
 
-    // Check for connection errors
+  
     if ($mysqli->connect_error) {
         die("Connection failed: " . $mysqli->connect_error);
     }
 
-    // Close the connection when done
+    
     $mysqli->close();
 ?>
